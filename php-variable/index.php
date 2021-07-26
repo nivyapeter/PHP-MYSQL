a3282ef88dca8f2992a0acf47d204dc478fe08fc
nivya <nivyapeter94@gmail.com>
2021-07-26 18:36:33 +0530

    php variable

---
+++ php-variable/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+$text = "icecreame";
+$text2 =  "choclate";
+echo "I love" . $text ."<br>";
+echo "but she loves" . $text2;
+?>
+
+</body>
+</html>
