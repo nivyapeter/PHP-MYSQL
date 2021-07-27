768f714cfc8675ba4b91e88ecbab861a715b7ec8
nivya <nivyapeter94@gmail.com>
2021-07-27 16:00:07 +0530

    math

---
+++ php math/math.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+// echo abs(-100);
+// echo pow(2, 4);
+// echo sqrt(144);
+// echo max(2,10);
+// echo min(2,10);
+// echo floor(3.3);
+echo ceil(3.3);
+?>  
+
+</body>
+</html>
