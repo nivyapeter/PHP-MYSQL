2ed1531e496a5b863a40054a4ebee142599c7a19
nivya <nivyapeter94@gmail.com>
2021-07-28 21:34:56 +0530

    construct-function

--- php-classes & objects/class.php
+++ php-classes & objects/class.php
@@ -7,13 +7,24 @@
     var $title;
     var $author;
     var $pages;
+
+  function __construct()
+  {
+    echo "New Book Created <br>";
   }
-  $book1 = new Book ;
+  }
+  
+  $book1 = new Book("mike") ;
   $book->title = "Harry Potter";
   $book->author = "JK Rowling";
   $book->pages = "400";
 
-  echo $book1->title;
+  $book2 = new Book("Tom") ;
+  $book2->title = "Lord of the Rings";
+  $book2->author = "Tolkiem";
+  $book2->pages = "700";
+
+  
 
 ?>  
 
