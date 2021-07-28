4bf1568ce2422aa08e4076f94b2e53b5db1ffaf0
nivya <nivyapeter94@gmail.com>
2021-07-28 19:10:13 +0530

    include

---
+++ php-include/include.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title></title>
+  </head>
+  <body>
+    <?php include "header.html"?>
+    <p>hi all</p>
+    <?php include "footer.html"?>
+  </body>
+</html>
