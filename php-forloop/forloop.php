2a978332146be08d07d259d0dd21fd3481b3d98c
nivya <nivyapeter94@gmail.com>
2021-07-28 16:56:23 +0530

    forloop

---
+++ php-forloop/forloop.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$luckyNumbers = array(4,8,14,32,44,45);
+for($i = 0; $i < count($luckyNumbers); $i++){
+  echo "$luckyNumbers[$i] <br>";
+}
+?>  
+
+</body>
+</html>
