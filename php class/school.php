89fafe21ca554ba9b19255a6f53661dd2741710f
nivya <nivyapeter94@gmail.com>
2021-08-09 16:22:49 +0530

    updated code

--- php class/school.php
+++ php class/school.php
@@ -6,12 +6,8 @@ class School {
   public $studentname;
   public $class;
   public $rollno;
-  public $classteacher;
-  public $school;
-  public $place;
-
-
-  public function __construct($studentname,$class,$rollno,$classteacher,$school,$place){
+ 
+  public function __construct($studentname,$class,$rollno){
       $this->studentname = $studentname;
       $this->class= $class;
       $this->rollno= $rollno;
@@ -29,22 +25,15 @@ class Oft extends School {
       echo "OFT students details";
       echo "<br>";
     }
+    
 }
-class Dbdo extends School {
-   public function message() {
-     echo "DBDO students details";
-     echo "<br>";
-   }
  
-}
+  
 
-$student= new Oft('vyshnav','plus one','1234','Nivya k Peter','GVHS(THS)','sulthan bathery');
-echo $student->message();
-echo $student-> displaystudentdetails();
-echo "<br>"
+$student1= new Oft('vyshnav','plus one','1234','Nivya k Peter','GVHS(THS)','sulthan bathery');
 
-// $student2= new Dbdo('Shyam','plus one','DBDO');
-// echo $student2-> message();
-// echo $student2-> displaystudentdetails();
+echo $student1->message();
+echo $student1-> displaystudentdetails();
+echo "<br>";
 
 ?>
