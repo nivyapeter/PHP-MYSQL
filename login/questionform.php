0d809a3451b7b33cbb33be42aab870bea01b94f2
nivya <nivyapeter94@gmail.com>
2021-08-10 18:00:05 +0530

    online exam

--- login/questionform.php
+++ login/questionform.php
@@ -8,10 +8,10 @@
     <title>Login form</title>
   </head>
   <body>
- <h3 style="height:49vh; font-size:20px">QUESTIONS</h3>
+ <h3 style="height:8vh; font-size:20px">QUESTIONS</h3>
  <div class="container1">
    <form>
-     <h4>What does PHP stand for? </h4>
+     <h4>1.What does PHP stand for? </h4>
      <div class="options">
        <input type="radio">Hypertext Preprocessor
      </div>
@@ -20,6 +20,26 @@
      </div>
      <div class="options">
        <input type="radio">PHP:Hypertext preprocessor
+     </div>
+     <h4>2.All variables in PHP start with which symbol?  </h4>
+     <div class="options">
+       <input type="radio"> #
+     </div>
+     <div class="options">
+       <input type="radio"> $
+     </div>
+     <div class="options">
+       <input type="radio"> &
+     </div>
+     <h4>3.What is the correct way to end a PHP statement?  </h4>
+     <div class="options">
+       <input type="radio"> ; 
+     </div>
+     <div class="options">
+       <input type="radio"> :
+     </div>
+     <div class="options">
+       <input type="radio"> %
      </div>
    </form>
 
