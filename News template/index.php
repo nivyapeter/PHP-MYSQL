245137dc4a0ec9ff40d657aeb4940e6dc28375b2
nivyapeter <nivyakpeter@gmail.com>
2021-11-17 23:42:05 +0530

    news page header section

---
+++ News template/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Login form</title>
+  </head>
+  <body>
+   <header id="news-head" class="header-site">
+        <div class="news-main-head">
+           <p class="news-title">News@Media</p>
+           <p class="Mysoor road, Sulthan bathery"></p>    
+           <nav class="nav-bar">
+               <ul class="main-nav">
+                   <li class="nav-items">
+                       <a href="#">Home</a>
+                    </li>
+                    <li class="nav-items">
+                       <a href="#">About</a>
+                    </li>
+                    <li class="nav-items">
+                       <a href="#">Contact</a>
+                    </li>
+               </nav>
+           </nav>   
+        </div>
+        
+   </header>
+       
+   </div>
+    
+
+  </body>
+</html>
+  
